2f3c19bc25bee4155ac16a7f0c087ab7c6b54fd8
yutafurunaga <126681269+yutafurunaga@users.noreply.github.com>
2023-07-07 14:30:10 +0000

    投稿一覧

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create' 
-  get 'lists/index'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
