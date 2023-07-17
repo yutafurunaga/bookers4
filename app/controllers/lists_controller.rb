3d5221bd0bb8db34bd2e547449bbe7c92a857f0a
yutafurunaga <126681269+yutafurunaga@users.noreply.github.com>
2023-07-17 14:44:34 +0000

    リダイレクト先変更

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,8 +9,7 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list.id)
     else
-      @lists = List.all
-      render :index #<= new から indexに変更
+      render :new
     end
   end
 
